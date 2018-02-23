1b841cbfcd932d2c85e482dad0d85624893ef27c
JennyLeeP <jennyleep@comcast.net>
2018-02-23 17:51:12 -0500

    Started work on Boulders and Shrubbary.

---
+++ src/main/java/com/cyborgJenn/alphaCentauri/blocks/BlockBoulders.java
@@ -0,0 +1,14 @@
+package com.cyborgJenn.alphaCentauri.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.MapColor;
+import net.minecraft.block.material.Material;
+
+public class BlockBoulders extends Block {
+
+	public BlockBoulders() 
+	{
+		super(Material.ROCK, MapColor.BLACK);
+	}
+
+}
--- src/main/java/com/cyborgJenn/alphaCentauri/blocks/BlockShrubbary.java
+++ src/main/java/com/cyborgJenn/alphaCentauri/blocks/BlockShrubbary.java
@@ -1,8 +1,43 @@
 package com.cyborgJenn.alphaCentauri.blocks;
 
-import net.minecraft.block.BlockBush;
-import net.minecraftforge.common.IPlantable;
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class BlockShrubbary extends BlockBush  implements IPlantable{
+public class BlockShrubbary extends Block
+{
+	protected static final AxisAlignedBB SHRUBBARY_AABB = new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 4.0D, 1.0D);
+	
+	public BlockShrubbary() 
+	{
+		super(Material.PLANTS);
+		this.blockSoundType = SoundType.PLANT;
+	}
+	@Override
+	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos)
+	{
+		return SHRUBBARY_AABB;
+	}
+	@Override
+    @SideOnly(Side.CLIENT)
+    public boolean shouldSideBeRendered(IBlockState blockState, IBlockAccess worldIn, BlockPos pos, EnumFacing side) {
+        return false;
+    }
 
+    @Override
+    public boolean isBlockNormalCube(IBlockState blockState) {
+        return false;
+    }
+
+    @Override
+    public boolean isOpaqueCube(IBlockState blockState) {
+        return false;
+    }
 }
